c55092265943949a35f5cc624d3482a62817ba8c
Koreikas <anton.kotelkov@mail.ru>
2020-04-29 23:42:37 +0300

    Create ConsoleApplication1.cpp
    11

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,49 @@
+﻿
+
+#include<iostream>
+using namespace std;
+int k = 1;
+int SortedLine(int** a, int n, int m);
+
+int main() {
+	int m=0,n;
+	cout << "input size of array:";
+	cin >> n;
+	cin >> m;
+	int** arr = new int* [n];
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = new int[n];
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++) 
+		{
+			cin >> arr[i][j];
+		}
+	}
+	cout << SortedLine(arr, n, m);
+	for (int i = 0; i < n; i++) delete[]arr[i];
+	delete[]arr;
+	return 0;
+}
+
+int SortedLine(int** a, int n, int m) {
+	for (int i = 0; i < n; i++) 
+	{
+		for (int j = 1; j < m; j++) 
+		{
+			if(a[i][j-1]>a[i][j])
+			{
+				k++;
+			}
+
+		}
+		if (k == m)
+		{
+			return i+1;
+		}
+		else k = 1;
+	}
+	return -1;
+}
