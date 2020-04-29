7f75a2c144a483c8f38ca185d9920ed81c5fede2
Koreikas <anton.kotelkov@mail.ru>
2020-04-30 02:21:21 +0300

    unity project test
    lknlkn

---
+++ New Unity Project/Assets/Scripts/GAME.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class GAME : MonoBehaviour
+{
+    public Text scoretext;
+    public Text HP;
+    public GameObject shop;
+    public GameObject kon;
+    private int score;
+    int bonus = 1;
+    public int hp = 1000;
+    public void Start()
+    {
+        shop.SetActive(false);
+        kon.SetActive(false);
+    }
+    public void shopOP()
+    {
+        shop.SetActive(!(shop.activeSelf));
+    }
+    public void Sila()
+    {
+        if (score>=20)
+        {
+            bonus += 1;
+        }
+        else
+        {
+            Debug.Log("Не зватает денег!");
+        }
+    }
+    public void ClickOn()
+    {
+        score += bonus;
+        scoretext.text = score + "$";
+        hp -= bonus;
+        HP.text = hp+"";
+    }
+
+
+    private void FixedUpdate()
+    {
+        if (hp <= 0) kon.SetActive(true);
+    }
+}
